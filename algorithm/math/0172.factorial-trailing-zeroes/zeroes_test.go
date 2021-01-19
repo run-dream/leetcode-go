826a3b1cba9ffa82be0ff24560c8589085ac6116
run-dream <837778355@qq.com>
2021-01-19 08:08:14 +0800

    feat: problem0172.factorial-trailing-zeroes

---
+++ algorithm/math/0172.factorial-trailing-zeroes/zeroes_test.go
@@ -0,0 +1,12 @@
+package problem0172
+
+import "testing"
+
+func TestZeroes(t *testing.T) {
+	//t.Log(trailingZeroes(0))
+	//t.Log(trailingZeroes(3))
+	//t.Log(trailingZeroes(10))
+	//t.Log(trailingZeroes(20))
+	t.Log(trailingZeroes(26))
+	t.Log(trailingZeroes(30))
+}
