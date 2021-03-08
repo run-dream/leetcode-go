cb92bd3b6ad325ecab06d420be1d0550abf3be33
run-dream <837778355@qq.com>
2021-03-08 08:35:00 +0800

    problem0144.binary-tree-preorder-traversal

---
+++ data-structure/tree/0144.binary-tree-preorder-traversal/README.md
@@ -0,0 +1,4 @@
+[144. Binary Tree Preorder Traversal](https://leetcode.com/problems/binary-tree-preorder-traversal/)
+
+### 思路
+前序遍历
---
+++ data-structure/tree/0144.binary-tree-preorder-traversal/solve.go
@@ -0,0 +1,18 @@
+package problem0144
+
+//TreeNode 树节点
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	result := []int{root.Val}
+	result = append(result, preorderTraversal(root.Left)...)
+	result = append(result, preorderTraversal(root.Right)...)
+	return result
+}
