0b38276b9cb43dc2292d2c3ff202dfd370e0cabb
run-dream <837778355@qq.com>
2021-03-20 08:39:25 +0800

    problem0231.power-of-two

---
+++ data-structure/bitwise/0231.power-of-two/README.md
@@ -0,0 +1,4 @@
+[231. Power of Two](https://leetcode.com/problems/power-of-two/description/)
+
+### 思路
+n & （n-1）== 0 筛选出只有最后一位
---
+++ data-structure/bitwise/0231.power-of-two/solve_test.go
@@ -0,0 +1,10 @@
+package problem0231
+
+import "testing"
+
+func TestSolve(t *testing.T) {
+	t.Log(isPowerOfTwo(-1))
+	t.Log(isPowerOfTwo(1))
+	t.Log(isPowerOfTwo(2))
+	t.Log(isPowerOfTwo(3))
+}
