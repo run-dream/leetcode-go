57e02b2bb91694ec632e02ca70f4251631f5ef66
run-dream <837778355@qq.com>
2021-04-22 08:15:09 +0800

    problem0400.nth-digits

---
+++ algorithm/math/0400.nth-digit/README.md
@@ -0,0 +1,4 @@
+[400. Nth Digit](https://leetcode.com/problems/nth-digit/)
+
+### 思路
+从0开始构建序列，直到序列长度超过n
---
+++ algorithm/math/0400.nth-digit/solve_test.go
@@ -0,0 +1,9 @@
+package problem0400
+
+import "testing"
+
+func TestSolve(t *testing.T) {
+	t.Log(findNthDigit(10))
+	t.Log(findNthDigit(3))
+	t.Log(findNthDigit(11))
+}
