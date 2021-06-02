dfb4d55976c45941dc910cc4c3e2c4ba587fe5a4
run-dream <837778355@qq.com>
2021-06-02 22:21:33 +0800

    remove golang-collection

--- data-structure/tree/post-order-traversal/solve.go
+++ data-structure/tree/post-order-traversal/solve.go
@@ -2,8 +2,6 @@ package post_order_traversal
 
 import (
 	"fmt"
-
-	"github.com/golang-collections/collections/stack"
 )
 
 type Node struct {
@@ -16,26 +14,27 @@ func postOrder(root *Node) {
 	if root == nil {
 		return
 	}
-	stack1 := stack.New()
+	stack := []*Node{}
 	cur := root
 	// 需要判断上次访问的节点是位于左子树，还是右子树。若是位于左子树，则需跳过根节点，先进入右子树，再回头访问根节点；若是位于右子树，则直接访问根节点
 	var prev *Node
 	for cur != nil {
-		stack1.Push(cur)
+		stack = append(stack, cur)
 		cur = cur.left
 	}
-	for stack1.Len() > 0 {
-		cur = stack1.Pop().(*Node)
+	for len(stack) > 0 {
+		cur =  stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
 		//一个根节点被访问的前提是：无右子树或右子树已被访问过
 		if cur.right == nil || cur.right == prev {
 			fmt.Println(cur.val)
 			// 修改最近被访问的节点
 			prev = cur
 		} else {
-			stack1.Push(cur)
+			stack = append(stack, cur)
 			cur = cur.right
 			for cur != nil {
-				stack1.Push(cur)
+				stack = append(stack, cur)
 				cur = cur.right
 			}
 		}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module leetcode-go
 
 go 1.13
-
-require github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
