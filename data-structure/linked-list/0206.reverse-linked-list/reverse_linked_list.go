f9c3bcaf4aa31e90fa4e422b2bd1b91181ffb435
run-dream <837778355@qq.com>
2021-08-17 23:25:29 +0800

    reverse_linked_list

--- data-structure/linked-list/0206.reverse-linked-list/reverse_linked_list.go
+++ data-structure/linked-list/0206.reverse-linked-list/reverse_linked_list.go
@@ -8,21 +8,29 @@ type ListNode struct {
 
 func reverseList(head *ListNode) *ListNode {
 	var newHead *ListNode
-	for head != nil {
-		next := head.Next
-		head.Next = newHead
-		newHead = head
-		head = next
+	cur := head
+	for cur != nil {
+		// 要处理的下个指针
+		next := cur.Next
+		// 反转指针
+		cur.Next = newHead
+		// 新的队头
+		newHead = cur
+		// 更新指针
+		cur = next
 	}
 	return newHead
 }
 
 func reverseListRecu(head *ListNode) *ListNode {
+	// 定义基准情况
 	if head == nil || head.Next == nil {
 		return head
 	}
 	newHead := reverseListRecu(head.Next)
+	// head.Next 是 newHead 的最后一个节点
 	head.Next.Next = head
+	// 清空 head.Next 指针
 	head.Next = nil
 	return newHead
 }
